1980bb6e7c28b03305ee2c9cb3a0be6255475eda
cprEugeneBen <159821261+cprEugeneBen@users.noreply.github.com>
2024-02-15 15:01:26 -0800

    starting the chrome extension
    Figure out how to load the page as an iframe. Moving all the files into the extension folder

--- chrome-extension/scripts/testing.js
+++ chrome-extension/scripts/testing.js
@@ -1,1 +1,5 @@
-
+let frame = document.createElement('iframe');
+frame.id ='checkin-frame';
+frame.src = chrome.runtime.getURL('/checkin-form/checkin.html');
+let inject = document.getElementById('customer');
+inject.appendChild(frame);
