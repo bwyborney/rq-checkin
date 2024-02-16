4a784ac733a2e14b6d0bdc495596a65163ee7529
Ben Wyborney - CPR Eugene <159821261+cprEugeneBen@users.noreply.github.com>
2024-02-16 11:39:19 -0800

    workin on it

---
+++ config-page/config.js
@@ -0,0 +1,57 @@
+// This will be the default configuration until changed
+let config = {
+    columnA: {
+        values: [
+            "Front cracked?",
+            "Back cracked?",
+            "Touch sensitivity",
+            "Display visibility",
+            "Biometric scanner",
+            "Proximity sensor",
+            "Ear speaker",
+            "Loudspeaker",
+            "Microphones",
+            "Vibration",
+            "Cellular signal",
+            "WiFi and Bluetooth",
+            "Side buttons",
+            "Rear camera",
+            "Front camera",
+            "Flashlight",
+            "Battery health"
+        ]
+    },
+    columnB: {
+        format: [
+            ["Yes", "No"],
+            ["Yes", "No"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["Pass", "Fail", "Not testable"],
+            ["percent"]
+        ],
+        values: [
+            999,
+            
+        ]
+    },
+    columnC: {
+        format: [
+            [""]
+        ],
+        values: [
+            []
+        ]
+    }
+};
