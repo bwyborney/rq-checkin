37ee4cf2fd12b03aca605e6405d1f9be268bccf6
Ben Wyborney - CPR Eugene <159821261+cprEugeneBen@users.noreply.github.com>
2024-05-08 13:12:29 -0700

    fixed mutation observer

--- chrome-extension/configInit.js
+++ chrome-extension/configInit.js
@@ -3,4 +3,6 @@
 let watch = document.getElementsByClassName('c-customFieldGroup')[0];
 const config = {childList: true, attributes: true};
 const observer = new MutationObserver(spawnConfigButton);
-observer.observe(watch, config);
+if (document.getElementsByClassName('c-customFieldGroup')[0]) {
+    observer.observe(watch, config);
+}
